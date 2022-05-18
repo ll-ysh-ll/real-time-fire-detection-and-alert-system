6fcdd96c59b2a4724544442b4fa4f03fc040f08b
Yash Kolekar <yashkolekar008@gmail.com>
2022-05-18 18:44:32 +0530

    Image and Video Detection using Streamlit

---
+++ demo.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import cv2
+from PIL import Image,ImageEnhance
+import numpy as np
+import pandas as pd
+import glob
+import random
+import os
+import time
+import pandas as pd
+import pydub
+import asyncio
+import logging
+import queue
+import threading
+import urllib.request
+from pathlib import Path
+from typing import List, NamedTuple
+from objectdetection import *
+from video_detection import *
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore
+import pydub
+import streamlit as st
+from aiortc.contrib.media import MediaPlayer
+import av
+
+
+@st.cache(persist=True)
+def load_image(img):
+    im = Image.open(img)
+    return im
+
+
+# Threshold Values
+#Conf_threshold = 0.4
+#NMS_threshold = 0.4
+
+# Colours
+COLORS = [(0, 255, 0), (0, 0, 255), (255, 0, 0),(255, 255, 0), (255, 0, 255), (0, 255, 255),(255, 255, 255)]
+
+@st.cache(persist=True)
+def txt2str(text):
+    txt=""
+    for i in text:
+        txt+=i+"\t"
+    return txt
+
+
+def main():
+    """
+    SeeFood
+    """
+    st.title('Fire Detection 🤖')
+    #st.text('Yolov4 and OpenCV')
+
+    menu = ['Image','Video']
+    choice = st.sidebar.selectbox('Menu',menu)
+
+    if choice == 'Image':
+        st.subheader('Image Detection')
+        with st.spinner('Wait for the Weights and Configuration files to load'):
+            time.sleep(1)
+        st.success("Done!")
+        image_file = st.file_uploader("Upload Image",type=['jpg', 'png', 'jpeg'])
+
+        if image_file is not None:
+            input_image = Image.open(image_file)
+            st.text('Original Image')
+            # st.write(type(input_image))
+            st.image(input_image)
+
+        # Food Detection
+        if st.button("Process"):
+            result_img,text = yolo_objectdetection(input_image)
+            st.image(result_img)
+            # txt=txt2str(text)
+            if text:
+                st.error("Fire Detected")
+
+            if not text:
+                st.success('No Fire')
+
+    if choice == 'Video':
+        video_detection()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ objectdetection.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+def yolo_objectdetection(image):
+	img = np.array(image)
+	with open('obj.names', 'r') as f:
+	    classes = f.read().splitlines()
+	net = cv2.dnn.readNetFromDarknet('yolov4-custom.cfg', 'backup/yolov4-custom_best.weights')
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+	model = cv2.dnn_DetectionModel(net)
+	model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
+	classIds, scores, boxes = model.detect(img, confThreshold=0.1, nmsThreshold=0.3)
+	txt=[]
+	for (classId, score, box) in zip(classIds, scores, boxes):
+	    cv2.rectangle(img, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
+	                  color=(0, 255, 0), thickness=2)
+	 
+	    text = '%s: %.2f' % (classes[classId[0]], score)
+	    txt.append(text)
+	    cv2.putText(img, text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
+	                color=(0, 255, 0), thickness=2)
+	color_img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	cv2.imwrite("prediction.jpg", color_img)
+	return img,txt
