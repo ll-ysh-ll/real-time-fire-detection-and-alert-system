be6997aa4c389ff0df7d5c9b1e6bf6a1aae3fbd6
Yash Kolekar <yashkolekar008@gmail.com>
2022-11-19 15:16:43 +0530

    weight_downloader refactor

--- weight_downloader.py
+++ weight_downloader.py
@@ -1,11 +1,15 @@
 import os
 
 def download_weight():
-    FILE_LIST = os.listdir("./YOLOv4/backup")
+
+
+    isFound=os.path.isfile("./YOLOv4/backup/yolov4-custom_best.weights")
+
     
-    if not "yolov4-custom_best.weights" in FILE_LIST:
+    if not isFound:
         print("Downloading...")
         os.system('wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=1K0kaVCRTNRDaYdnRaX9MUGu4OZf3G1cv" -O- | sed -rn "s/.*confirm=([0-9A-Za-z_]+).*/\1\n/p")&id=1K0kaVCRTNRDaYdnRaX9MUGu4OZf3G1cv" -O yolov4-custom_best.weights && rm -rf /tmp/cookies.txt')
         print("Download Completed")
+        os.system('mkdir ./YOLOv4/backup/')
         os.system('mv ./yolov4-custom_best.weights ./YOLOv4/backup/')
         print("Moved to backup")
